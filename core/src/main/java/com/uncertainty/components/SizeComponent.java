c342b09efedc4cee442aaf326745a191be0e284a
Alex Ianta <aianta@dal.ca>
2020-07-09 13:34:43 -0300

    Revert "added 3d player and walls"
    going back to pure 2d
    
    This reverts commit 6e13124b394965dcbe97b102128554fa9ed7c5f8.

--- core/src/main/java/com/uncertainty/components/SizeComponent.java
+++ core/src/main/java/com/uncertainty/components/SizeComponent.java
@@ -3,7 +3,6 @@ package com.uncertainty.components;
 import com.badlogic.ashley.core.Component;
 
 public class SizeComponent implements Component {
-    public float width;
-    public float height;
-    public float depth;
+    public int width;
+    public int height;
 }
