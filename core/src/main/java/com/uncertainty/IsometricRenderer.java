940971879717b467ed281ecb0db2da262282b9da
Alex Ianta <aianta@dal.ca>
2022-07-05 07:43:13 -0600

    actual progress towards creating a world

---
+++ core/src/main/java/com/uncertainty/IsometricRenderer.java
@@ -0,0 +1,61 @@
+package com.uncertainty;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.util.Random;
+
+public class IsometricRenderer {
+
+    public static final int TILE_WIDTH = 32;
+    public static final int TILE_HEIGHT = 32;
+
+    private Texture dirtBlock;
+    private Random random = new Random();
+    private int [] [] topLayer = new int[16][16];
+
+    public IsometricRenderer(){
+        dirtBlock = new Texture(Gdx.files.internal("dirt-cube.png"));
+
+        for (int i =  0; i <  16; i++){
+            for(int j = 0; j < 16; j++){
+                if(random.nextBoolean()){
+                    topLayer[i][j] = 1;
+                }else{
+                    topLayer[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public void drawLayer(SpriteBatch batch, int layer){
+        if(layer == 0){
+            drawGround(batch);
+            return;
+        }
+
+        for (int row = 15; row >= 0; row--){
+            for(int col = 15; col >= 0; col--){
+                float x = (col - row) * (TILE_WIDTH/2f);
+                float y = (col + row) * (TILE_HEIGHT/4f)+(TILE_HEIGHT*layer/2f);
+
+                if (topLayer[row][col] == 1){
+                    batch.draw(dirtBlock, x,y,TILE_WIDTH,TILE_HEIGHT);
+                }
+            }
+        }
+    }
+
+    public void drawGround(SpriteBatch batch){
+        for (int row = 15; row >= 0; row--){
+            for(int col = 15; col >= 0; col--){
+                float x = (col - row) * (TILE_WIDTH/2f);
+                float y = (col + row) * (TILE_HEIGHT/ 4f);
+
+                batch.draw(dirtBlock, x,y,TILE_WIDTH,TILE_HEIGHT);
+            }
+        }
+    }
+
+}
--- core/src/main/java/com/uncertainty/UncertaintyGame.java
+++ core/src/main/java/com/uncertainty/UncertaintyGame.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
@@ -33,6 +34,9 @@ import java.util.stream.Stream;
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class UncertaintyGame extends ApplicationAdapter {
 
+    public static final int VIEWPORT_WIDTH = 320 * 4;
+    public static final int VIEWPORT_HEIGHT = 180 * 4;
+
     public static final int MAP_HEIGHT = 27;
     public static final int MAP_WIDTH = 27;
     public static final int CHUNK_HEIGHT = MAP_HEIGHT/3;
@@ -43,6 +47,7 @@ public class UncertaintyGame extends ApplicationAdapter {
 
     private OrthographicCamera camera;
     private ShapeRenderer shapeRenderer;
+    private IsometricRenderer renderer;
     private MovementSystem movementSystem;
     private Engine engine;
 
@@ -55,6 +60,7 @@ public class UncertaintyGame extends ApplicationAdapter {
 
     private BitmapFont font;
     private SpriteBatch batch;
+    private SpriteBatch textOverlay;
 
     private Chunk [][] world;
     public static int currentDepth = 0;
@@ -66,49 +72,52 @@ public class UncertaintyGame extends ApplicationAdapter {
     public int currAngle = 0;
 
     Texture img;
-    TiledMap tiledMap;
-    IsometricTiledMapRenderer tiledMapRenderer;
+    Sprite sprite;
+    List<Sprite> sprites = new ArrayList<>();
+//    TiledMap tiledMap;
+//    IsometricTiledMapRenderer tiledMapRenderer;
 
     public void create(){
 
+
+
         //Setup isometric view
         camera = new OrthographicCamera();
-        camera.setToOrtho(false, 250,250);
-        matrix.setToRotation(new Vector3(1,0,0), 90);
+        camera.setToOrtho(false, VIEWPORT_WIDTH,VIEWPORT_HEIGHT);
+        camera.position.set(VIEWPORT_WIDTH/2 - 500, VIEWPORT_HEIGHT/2, 10);
 
-
-
-        tiledMap = new TmxMapLoader().load("testMap.tmx");
-        tiledMapRenderer = new IsometricTiledMapRenderer(tiledMap);
 
         batch = new SpriteBatch();
+        textOverlay = new SpriteBatch();
+
         font = new BitmapFont();
-        shapeRenderer = new ShapeRenderer();
 
-        //Create world
-        world = generateWorld();
+        renderer = new IsometricRenderer();
 
-        //Initalize entity system
-        engine = new Engine();
+//        //Create world
+//        world = generateWorld();
+//
+//        //Initalize entity system
+//        engine = new Engine();
+//
+//        //Register the movement system
+//        movementSystem = new MovementSystem();
+//        engine.addSystem(movementSystem);
 
-        //Register the movement system
-        movementSystem = new MovementSystem();
-        engine.addSystem(movementSystem);
+//        //Create a player
+//        player = new Entity();
+//        var startingPositon = new PositionComponent();
+//        startingPositon.x = MAP_WIDTH/2;
+//        startingPositon.y = MAP_HEIGHT/2;
 
-        //Create a player
-        player = new Entity();
-        var startingPositon = new PositionComponent();
-        startingPositon.x = MAP_WIDTH/2;
-        startingPositon.y = MAP_HEIGHT/2;
+//        player.add(startingPositon);
+//        player.add(new VelocityComponent());
 
-        player.add(startingPositon);
-        player.add(new VelocityComponent());
-
-        var playerSize = new SizeComponent();
-        playerSize.width = 1;
-        playerSize.height = 1;
-        player.add(playerSize);
-        engine.addEntity(player);
+//        var playerSize = new SizeComponent();
+//        playerSize.width = 1;
+//        playerSize.height = 1;
+//        player.add(playerSize);
+//        engine.addEntity(player);
 
         Gdx.input.setInputProcessor(new CameraController(camera));
         Gdx.app.setLogLevel(Application.LOG_DEBUG);
@@ -125,38 +134,43 @@ public class UncertaintyGame extends ApplicationAdapter {
         Gdx.gl.glClearColor(0,0,0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-        //camera.position.set(cameraAngles[currAngle]);
-        camera.update();
-        tiledMapRenderer.setView(camera);
-        tiledMapRenderer.render();
+        batch.setProjectionMatrix(camera.combined);
+
+
+        batch.begin();
+        int layerIndex = 0;
+        while (layerIndex <= currentDepth){
+            renderer.drawLayer(batch, layerIndex);
+            layerIndex++;
+        }
+        batch.end();
+
 
         //Update entities
-        engine.update(Gdx.graphics.getDeltaTime());
+        //engine.update(Gdx.graphics.getDeltaTime());
 
         //Process Controls
         if(Gdx.input.isKeyPressed(Input.Keys.W)) camera.translate(0,1,0);
         if(Gdx.input.isKeyPressed(Input.Keys.A)) camera.translate(-1, 0,0);
         if(Gdx.input.isKeyPressed(Input.Keys.S)) camera.translate(0,-1,0);
         if(Gdx.input.isKeyPressed(Input.Keys.D)) camera.translate(1,0,0);
-        if(Gdx.input.isKeyPressed(Input.Keys.L))
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) velocity.get(player).x -= MovementSystem.MAX_VELOCITY/8;
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) velocity.get(player).x += MovementSystem.MAX_VELOCITY/8;
-        if(Gdx.input.isKeyPressed(Input.Keys.UP)) velocity.get(player).y -= MovementSystem.MAX_VELOCITY/8;
-        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) velocity.get(player).y += MovementSystem.MAX_VELOCITY/8;
-        if(Gdx.input.isKeyJustPressed(Input.Keys.G)) showGrid = !showGrid; //Toggle grid
-        if(Gdx.input.isKeyJustPressed(Input.Keys.R)) world = generateWorld(); //Generate a new world
-        if(Gdx.input.isKeyJustPressed(Input.Keys.PERIOD)) {
-            world = rotate();
-        }
+//        if(Gdx.input.isKeyPressed(Input.Keys.L))
+//        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) velocity.get(player).x -= MovementSystem.MAX_VELOCITY/8;
+//        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) velocity.get(player).x += MovementSystem.MAX_VELOCITY/8;
+//        if(Gdx.input.isKeyPressed(Input.Keys.UP)) velocity.get(player).y -= MovementSystem.MAX_VELOCITY/8;
+//        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) velocity.get(player).y += MovementSystem.MAX_VELOCITY/8;
+
 
 
         //Render FPS
-        batch.begin();
-        font.draw(batch, "FPS: " + Gdx.graphics.getFramesPerSecond(), 10,20);
-        font.draw(batch, "Depth: " + UncertaintyGame.currentDepth, 10, 40);
-        font.draw(batch, "Viewport (height: " + camera.viewportHeight + ", width: " + camera.viewportWidth + ")", 10, 60);
-        font.draw(batch, "Camera Position (x: "+camera.position.x+" y: "+ camera.position.y+" z: "+camera.position.z+")", 10, 80);
-        batch.end();
+        textOverlay.begin();
+        font.draw(textOverlay, "FPS: " + Gdx.graphics.getFramesPerSecond(), 10,20);
+        font.draw(textOverlay, "Depth: " + UncertaintyGame.currentDepth, 10, 40);
+        font.draw(textOverlay, "Viewport (height: " + camera.viewportHeight + ", width: " + camera.viewportWidth + ")", 10, 60);
+        font.draw(textOverlay, "Camera Position (x: "+camera.position.x+" y: "+ camera.position.y+" z: "+camera.position.z+")", 10, 80);
+        textOverlay.end();
+
+        camera.update();
     }
 
     public BlockType [][] getLayer(int depth){
